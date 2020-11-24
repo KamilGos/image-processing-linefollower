fa8ecf83652b42d746700f66d4c6a9d5b974c805
KamilGos <kamil.gos00@gmail.com>
2020-11-24 16:59:51 +0100

    Vision system DONE

--- camera.py
+++ camera.py
@@ -8,8 +8,9 @@
 # The calling application tracks the frames_displayed
 
 # Let's use a repeating Timer for counting FPS
-import cv2
+import cv2 as cv2
 import threading
+from numpy import full, uint8
 
 class RepeatTimer(threading.Timer):
     def run(self):
@@ -141,10 +142,16 @@ class CSI_Camera:
             )
         )
 
+# Private class
+# Author: Kamil Goś
 
 class LineFollowerCamera():
-    def __init__(self, show_fps):
+    def __init__(self, show_fps, crop_left, crop_right, crop_top, crop_bottom):
         self.show_fps = show_fps
+        self.crop_left = crop_left
+        self.crop_right = crop_right
+        self.crop_top = crop_top
+        self.crop_bottom = crop_bottom
         self.DISPLAY_WIDTH=640
         self.DISPLAY_HEIGHT=360
         self.SENSOR_MODE_720=3
@@ -209,6 +216,14 @@ class LineFollowerCamera():
         else:
             print("Unable to open window: ", win_name)
 
+
+
+    def create_trackbar(self, win_name, tb_name, low, high):
+        cv2.createTrackbar(tb_name, win_name, low, high, nothing)
+
+    def get_trackbar_pos(self, tb_name, win_name):
+        return cv2.getTrackbarPos(tb_name, win_name)
+
     def close_camera(self):
         self.my_camera.stop()
         self.my_camera.release()
@@ -233,40 +248,61 @@ class LineFollowerCamera():
             self.plot_fps(frame)
         return frame
 
-    def extract_line(self, img):
-        print(img.shape)
-        img = img[150:340, 0:640]
-        mask = img.copy()
+    def extract_line(self, img, TB_THRESH, TB_SHOW):
+        oryginal = img.copy()
+        cv2.line(oryginal, (self.crop_left, self.crop_top), (self.crop_right, self.crop_top), (0, 0, 255), 1)
+        cv2.line(oryginal, (self.crop_left, self.crop_bottom), (self.crop_right, self.crop_bottom), (0, 0, 255), 1)
+
+        img = img[self.crop_top:self.crop_bottom, self.crop_left:self.crop_right]
         frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         frame = cv2.GaussianBlur(frame, (5,5), 0)
-        _, frame = cv2.threshold(frame, 100, 255, cv2.THRESH_BINARY_INV)
-        # mask = cv2.erode(frame, None, iterations=3)
-        # mask = cv2.dilate(mask, None, iterations=3)
-        contours, hierarchy = cv2.findContours(frame, 1, cv2.CHAIN_APPROX_NONE)
-        # mask=frame.copy()
-        cv2.drawContours(mask, contours, -1, (0, 255, 0), 1)
+        _, frame = cv2.threshold(frame, 105, 255, cv2.THRESH_BINARY_INV)
 
-        return frame, mask
+        contours, hierarchy = cv2.findContours(frame, 1, cv2.CHAIN_APPROX_SIMPLE)
         
+        if len(contours) > 0:
+            c = max(contours, key=cv2.contourArea)
+            M = cv2.moments(c)
+            try:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])+self.crop_top
+            except:
+                print("Centroid error")
 
+            if TB_SHOW == 0:
+                cv2.drawContours(img, contours, -1, (0, 255, 0), 1)
+                cv2.line(oryginal, (cx, self.crop_top), (cx, self.crop_bottom), (255, 0, 0), 1)
+                cv2.line(oryginal, (self.crop_left, cy), (self.crop_right, cy), (255, 0, 0), 1)
+                cv2.circle(oryginal, (cx, cy), 5, (0,255,0), 3)
 
+        return img, oryginal
 
+def nothing(x):
+    pass    
 
 if __name__ == "__main__":
-    camera = LineFollowerCamera(True)
+    camera = LineFollowerCamera(show_fps=True,
+                                crop_left = 0,
+                                crop_right = 640,
+                                crop_top = 200,
+                                crop_bottom = 340)
     camera.initialize_camera()
     camera.open_new_window("ORYGINAL")
+    camera.create_trackbar("ORYGINAL", "SHOW", 0,2)
+    camera.create_trackbar("ORYGINAL", "THRESH", 1, 255)
+    
     camera.open_new_window("LINE EXTRACTION")
-    camera.open_new_window("TMP1")
-    # camera.open_new_window("TMP2")
     while True:
         try:
             frame = camera.read_frame()
-            camera.show_image("ORYGINAL", frame)
+            TB_SHOW = cv2.getTrackbarPos('SHOW', 'ORYGINAL')
+            TB_THRESH = cv2.getTrackbarPos('THRESH', 'ORYGINAL')
+            binary, oryginal  = camera.extract_line(frame, TB_THRESH, TB_SHOW)
+            if TB_SHOW == 0:
+                camera.show_image("ORYGINAL",oryginal)
 
-            bin, adapt = camera.extract_line(frame)
-            camera.show_image("LINE EXTRACTION",bin)
-            camera.show_image("TMP1",adapt)
+            camera.show_image("LINE EXTRACTION",binary)
+            
             keyCode = cv2.waitKey(5) & 0xFF
             # Stop the program on the ESC key
             if keyCode == 27:
